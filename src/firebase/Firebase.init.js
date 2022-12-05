2b765bd51420b13a6aa806ae4e8305b8928e9433
salauddinAmir <mdsalauddinahmed.amir@gmail.com>
2022-12-05 18:29:45 +0600

    first commit

---
+++ src/firebase/Firebase.init.js
@@ -0,0 +1,14 @@
+ 
+import { initializeApp } from "firebase/app";
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyD3AjWeV7ekWEpI0EKb-ecs1LkWEt53D5E",
+  authDomain: "firbase-login-password.firebaseapp.com",
+  projectId: "firbase-login-password",
+  storageBucket: "firbase-login-password.appspot.com",
+  messagingSenderId: "669945172971",
+  appId: "1:669945172971:web:3e7aeddbf54bdfd85fbfae"
+};
+ 
+const app = initializeApp(firebaseConfig);
+export default app;
